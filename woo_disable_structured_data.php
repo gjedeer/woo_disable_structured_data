3b96f0651752cbcfad162af17d299b2202bf32a5
GDR! <gdr@gdr-ThinkPad-X230.gdr.name>
2020-08-27 09:52:34 +0200

    Initial

---
+++ woo_disable_structured_data.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: woo_disable_structured_data
+ * Version: 1.0
+ * Plugin URI: http://www.hughlashbrooke.com/
+ * Description: Disable JSON-LD/structure/microdata from WooCommerce pages
+ * Author: Andrzej Godziuk
+ * Author URI: https://massivevps.net/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function woo_disable_output_structured_data() {
+	remove_action('wp_footer', array( WC()->structured_data, 'output_structured_data' ), 10); 
+	remove_action('woocommerce_email_order_details', array( WC()->structured_data, 'output_email_structured_data' ), 30);
+}
+add_action( 'init', 'woo_disable_output_structured_data' );
